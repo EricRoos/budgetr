7e442dfe93efe67987fcc5e5dbc180988d3473c2
Eric Roos <ericroos@hey.com>
2020-09-20 13:57:56 -0500

    cleanup views. refactor.

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,8 @@ class Project < ApplicationRecord
   def budget_remaining
     budget - (items.sum(:purchase_price_cents) / 100)
   end
+
+  def allocated_budget
+    item_groups.sum(:budget)
+  end
 end
