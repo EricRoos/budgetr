ef50695da26159f7d90fbf01ca7231e955423069
Eric Roos <ericroos@hey.com>
2020-10-06 16:20:28 -0500

    redirect to right place after delete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -61,7 +61,7 @@ class ItemsController < ApplicationController
     @item.destroy
     last_item = @item_group.items.last
     respond_to do |format|
-      format.html { redirect_to project_path(@project, anchor: "item-tr-#{last_item&.id}", shownItemGroup: @item_group.id), notice: 'Item was successfully destroyed.' }
+      format.html { redirect_to project_item_group_path(@project, @item_group), notice: 'Item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
