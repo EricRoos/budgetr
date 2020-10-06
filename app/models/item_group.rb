bd57898d668ae901cb8b26a658437d2768e79022
Eric Roos <ericroos@hey.com>
2020-10-06 17:38:11 -0500

    fix values

--- app/models/item_group.rb
+++ app/models/item_group.rb
@@ -4,10 +4,15 @@ class ItemGroup < ApplicationRecord
   has_rich_text :note
 
   def total_spent
-    Money.new(items.where(purchased: true).pluck(:purchase_price_cents).compact.sum)
+    Money.new(total_spent_on(items.where(purchased: true)))
   end
 
   def total_allocated
-    Money.new(items.pluck(:purchase_price_cents).compact.sum)
+    Money.new(total_spent_on(items))
   end
+
+  private
+    def total_spent_on(items)
+      items.pluck(:purchase_price_cents, :quantity).compact.map{ |i| i.inject(:*) }.sum
+    end
 end
