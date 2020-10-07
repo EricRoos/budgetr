d1cf1d6c8c93f293e1f71b26064d9246e683bd04
Eric Roos <ericroos@hey.com>
2020-10-06 20:51:25 -0500

    add total price

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
   monetize :purchase_price_cents, allow_nil: true
   has_rich_text :note
 
+  def total_price
+    return Money.new(0) unless purchase_price_cents.present?
+    purchase_price * quantity 
+  end
 end
