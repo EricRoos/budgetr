d57c8dc7ff7df0eba8b8f449dc25c99ee9f4c05b
Eric Roos <ericroos13@gmail.com>
2020-10-25 22:50:57 -0500

    refactor

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -1,3 +1,10 @@
 class BasePresenter
   include ActiveModel::Model
+
+  private
+
+  def entity_count(identifier, count)
+    return "No items" if count == 0
+    "#{count} #{count > 1 ? identifier.pluralize : identifier}"
+  end
 end
---
+++ app/presenters/item_groups/item_group.rb
@@ -0,0 +1,24 @@
+module ItemGroups
+  class ItemGroup < BasePresenter
+    attr_accessor :item_group
+
+    def item_group_name
+      item_group.name
+    end
+
+    def item_count
+      count = item_group.items.size
+      entity_count('item', count)
+    end
+
+    def metrics
+      [
+        { name: 'Total Budget', help_text: 'Total amount to spend on this room', value: total_budget }
+      ]
+    end
+
+    def total_budget
+      Money.new(100 * (item_group.budget || 0)).format
+    end
+  end
+end
