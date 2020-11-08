5c9210b57f861f05fbf1fc57b40123ec9fc1f040
Eric Roos <ericroos13@gmail.com>
2020-11-08 14:36:35 -0600

    add feature spec

---
+++ spec/features/contributor_management_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.feature "Contributor Management", type: :feature do
+  let(:current_user) {
+    project_owner
+  }
+
+  let(:project_owner) {
+    email = 'foo@test.com'
+    password = 'test123456'
+    User.create(email: email, password: password)
+  }
+
+  let(:project) do
+    project = Project.new(name: 'foo', budget: 1000)
+    project_owner.add_project(project)
+    project
+  end
+
+  before do
+    sign_in current_user
+  end
+
+  let(:new_contributing_user) { User.create(email: 'contrib@1.com', password: 'test123456') }
+  scenario 'Adds a contributor' do
+    visit edit_project_path(project)
+    click_on "Add Contributor"
+    fill_in "Email", with: new_contributing_user.email
+    click_on "Create Contributor"
+    expect(page).to have_content('Contributor was successfully created.')
+  end
+
+  scenario 'Fails to add a contributor' do
+    visit edit_project_path(project)
+    click_on "Add Contributor"
+    fill_in "Email", with: 'notfound@no.com'
+    click_on "Create Contributor"
+    expect(page).to have_content('1 error')
+    expect(page).to have_content('User must exist')
+
+  end
+
+end
