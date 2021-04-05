a094d9e39de7573794905d766e2e1424ef3786dc
Eric Roos <ericroos@hey.com>
2021-04-04 22:10:46 -0500

    try touch on lock

--- app/models/edit_lock.rb
+++ app/models/edit_lock.rb
@@ -1,4 +1,4 @@
 class EditLock < ApplicationRecord
-  belongs_to :lockable, polymorphic: true
+  belongs_to :lockable, polymorphic: true, touch: true
   belongs_to :locked_by, foreign_key: :user_id, class_name: 'User'
 end
